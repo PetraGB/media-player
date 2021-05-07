9d2f187210f26d14e64663fa90b651a1269ce694
Petra <petragb@gmail.com>
2021-05-07 11:27:52 +0200

    Update index.tsx

--- src/components/logic/Like/index.tsx
+++ src/components/logic/Like/index.tsx
@@ -4,25 +4,20 @@ import Heart from '../../elements/Heart';
 interface IProps {
   count: number;
   isActive?: boolean;
-  onClick: () => void;
+  onClick: (arg: boolean) => void;
 }
 
 function Like(props: IProps) {
   const [count, setCount] = useState<number>(props.count);
   const [isActive, setActive] = useState<boolean>(Boolean(props.isActive));
 
-  const like = () => {
-    if (isActive) {
-      return null;
-    }
-
-    props.onClick();
-    setActive(true);
-    setCount(count + 1);
-  };
+  useEffect(() => {
+    props.onClick(!isActive);
+    setCount(!isActive ? count - 1 : count + 1);
+  }, [isActive])
 
   return (
-    <Heart isActive={isActive} count={count} onClick={like} />
+    <Heart isActive={isActive} count={count} onClick={() => setActive(!isActive)} />
   );
 }
 
